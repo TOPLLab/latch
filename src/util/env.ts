447d3564e3975a74a4572817352809ddd5be7e97
Tom Lauwaerts <tom.lauwaerts@vub.be>
2025-12-15 13:24:11 +0100

    Adapt exit code to success

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,6 +1,6 @@
 import {homedir} from 'os';
 
-export const WABT: string = process.env.WABT ?? `${homedir()}/Documents/TOPL/plugin/WABT/build/`;
+export const WABT: string = process.env.WABT ?? `${homedir()}/Arduino/libraries/WARDuino/lib/wabt/build/`;
 
 export const EMULATOR: string = process.env.EMULATOR ?? `${homedir()}/Arduino/libraries/WARDuino/build-emu/wdcli`;
 
